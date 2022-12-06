dd0b51a79d225151d97161da20c0d7686a19467e
David López <davidlghellin@gmail.com>
2022-12-06 21:23:30 +0100

    [SALVO] [VITE] [REACT] Union con react+vite

--- salvo_tutorial/src/main.rs
+++ salvo_tutorial/src/main.rs
@@ -1,4 +1,6 @@
+use rust_embed::RustEmbed;
 use salvo::{prelude::*, serve_static::StaticDir};
+use salvo::serve_static::static_embed;
 
 #[handler]
 async fn hello_world() -> &'static str {
@@ -14,21 +16,29 @@ async fn service() -> &'static str {
     "service"
 }
 
+//mi carpeta de fronted se encuenta
+#[derive(RustEmbed)]
+#[folder = "../salvo_tutorial_client/client/dist/"]
+struct Assets;
+
 #[tokio::main]
 async fn main() {
-    let router = Router::new()
-        // Nos puede servir para crear API
-        .push(Router::new().path("").get(hello_world))
-        .push(Router::new().path("about").get(about))
-        .push(Router::new().path("service").get(service))
-        // y esto para servir el contenido estático
-        .push(
-            Router::with_path("<**path>").get(
-                StaticDir::new(["public"])
-                    .with_defaults("index.html")
-                    .with_listing(true),
-            ),
-        );
+    // let router = Router::new()
+    //     // Nos puede servir para crear API
+    //     .push(Router::new().path("").get(hello_world))
+    //     .push(Router::new().path("about").get(about))
+    //     .push(Router::new().path("service").get(service))
+    //     // y esto para servir el contenido estático
+    //     .push(
+    //         Router::with_path("<**path>").get(
+    //             StaticDir::new(["public"])
+    //                 .with_defaults("index.html")
+    //                 .with_listing(true),
+    //         ),
+    //     );
+
+    let router =
+        Router::with_path("<**path>").get(static_embed::<Assets>().with_fallback("index.html"));
 
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .serve(router)
---
+++ salvo_tutorial_client/README.md
@@ -0,0 +1,5 @@
+npm create vite
+cd client
+mpm install
+npm run dev
+npm run build
