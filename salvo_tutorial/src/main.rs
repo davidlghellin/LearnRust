f2c207a33e0335d9862e22b0cf7f7a13193a8557
David López <davidlghellin@gmail.com>
2022-12-06 19:52:39 +0100

    [SALVO] Varias urls

--- salvo_tutorial/src/main.rs
+++ salvo_tutorial/src/main.rs
@@ -4,9 +4,22 @@ use salvo::prelude::*;
 async fn hello_world() -> &'static str {
     "hola mundo"
 }
+
+#[handler]
+async fn about() -> &'static str {
+    "about"
+}
+#[handler]
+async fn service() -> &'static str {
+    "service"
+}
+
 #[tokio::main]
 async fn main() {
-    let router = Router::new().get(hello_world);
+    let router = Router::new()
+        .push(Router::new().path("").get(hello_world))
+        .push(Router::new().path("about").get(about))
+        .push(Router::new().path("service").get(service));
 
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .serve(router)
