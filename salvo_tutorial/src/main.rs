530b324dca637674e5c01818c1f6d4ab31444a15
David López <davidlghellin@gmail.com>
2022-12-06 19:46:37 +0100

    [SALVO] HolaMundo en puerto 3000

--- salvo_tutorial/src/main.rs
+++ salvo_tutorial/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use salvo::prelude::*;
+
+#[handler]
+async fn hello_world() -> &'static str {
+    "hola mundo"
+}
+#[tokio::main]
+async fn main() {
+    let router = Router::new().get(hello_world);
+
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .serve(router)
+        .await;
 }
