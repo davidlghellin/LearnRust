a14e09210892ff5ca0c563863f8b0256e6125283
David López <davidlghellin@gmail.com>
2022-12-26 12:34:52 +0100

    [THE-BOOK] [RUST] Cap04 Teoría principalmente

---
+++ learn_006_rust_lang_org/src/cap04/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // Reglas del ownership
+    /*
+    1. Cada valor en Rust tiene una variable, cada variable tiene un dueño.
+    2. Solo puede haber un dueño todo el tiempo.
+    3. Cuando el propietario sale del scope, el valor es borrado
+    */
+
+    // Reglas de la referencia
+    /*
+    1. En un momento dado, para una parte particular de datos en un
+
+    2. Referencias deben ser siempre válidas.
+    */
+}
