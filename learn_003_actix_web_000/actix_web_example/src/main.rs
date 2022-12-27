b2283ed84a5ac9c80967fd1e1872a0a9a8c4be4d
David López <davidlghellin@gmail.com>
2022-12-27 11:27:47 +0100

    [ACTIX] [LOG] [WEB] Log en el proyecto de actix

--- learn_003_actix_web_000/actix_web_example/src/main.rs
+++ learn_003_actix_web_000/actix_web_example/src/main.rs
@@ -1,6 +1,9 @@
 use actix_web::http::StatusCode;
 use actix_web::{web, App, Error, HttpResponse, HttpServer};
 
+use actix_web::middleware::Logger;
+use log::info;
+
 async fn home() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
@@ -9,8 +12,16 @@ async fn home() -> Result<HttpResponse, Error> {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().route("/", web::get().to(home)))
-        .bind("127.0.0.1:8888")?
-        .run()
-        .await
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+    info!("Activamos logs");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .route("/", web::get().to(home))
+    })
+    .bind("127.0.0.1:8888")?
+    .run()
+    .await
 }
