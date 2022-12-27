12e2b27a38ee8740d0611ac2e6c0b256ee51fad0
David López <davidlghellin@gmail.com>
2022-12-27 13:23:31 +0100

    [ACTIX] [WEB] Configuración por variables de entorno

--- learn_007_actix_web_001/src/main.rs
+++ learn_007_actix_web_001/src/main.rs
@@ -3,13 +3,17 @@ use actix_web::{web, App, HttpServer};
 use actix_web::middleware::Logger;
 use log::info;
 
-use learn_007_actix_web_001::infra::endpoints::config;
+use learn_007_actix_web_001::config::{read_config, Config};
+use learn_007_actix_web_001::infra;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
     info!("Activamos logs");
+
+    // Añadimos la lectura de la configuración
+    let configuracion: Config = read_config();
 
     HttpServer::new(|| {
         App::new()
@@ -21,10 +25,10 @@ async fn main() -> std::io::Result<()> {
                 // hacemos que todo esté dentro de /api
                 web::scope("/api")
                     // hemos añadido la configuración de la infra
-                    .configure(config),
+                    .configure(infra::endpoints::config),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((configuracion.host, configuracion.puerto))?
     .run()
     .await
 }
