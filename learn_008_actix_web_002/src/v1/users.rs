cd878dd17ad2b7f70acaff54aa2ea22e32a1ef3d
David López <davidlghellin@gmail.com>
2022-12-29 18:37:35 +0100

    [ACTIX] [API] [REST] [WEB] [TEST] Test con Mockall (no funciona)

--- learn_008_actix_web_002/src/v1/users.rs
+++ learn_008_actix_web_002/src/v1/users.rs
@@ -5,7 +5,7 @@ use actix_web::{
 };
 use uuid::Uuid;
 
-use crate::repository::{Repository};
+use crate::repository::Repository;
 // .service(web::resource("/user/{user_id}").route(web::get().to(get_user)))
 
 const PATH: &str = "/user";
@@ -41,4 +41,51 @@ pub async fn get<R: Repository>(user_id: web::Path<Uuid>, repo: web::Data<R>) ->
 
 fn path_config_handler(err: PathError, _req: &HttpRequest) -> actix_web::Error {
     actix_web::error::ErrorBadRequest(err)
+}
+
+#[cfg(test)]
+use mockall::{automock, mock, predicate::*};
+#[cfg_attr(test, automock)]
+trait MyTrait {
+    fn foo(&self, x: u32) -> u32;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::user::User;
+
+    use super::*;
+    // use mockall::predicate::*;
+    // use mockall::*;
+
+    #[test]
+    fn mytest() {
+        let mut mock = MockMyTrait::new();
+        mock.expect_foo().with(eq(4)).times(1).returning(|x| x + 1);
+        assert_eq!(5, mock.foo(4));
+    }
+
+    mock! {
+        CustomRepo{}
+        impl Repository for CustomRepo{
+            fn get_user(&self, user_id:&uuid::Uuid) -> Result<User,String>;
+        }
+    }
+
+    #[actix_rt::test]
+    async fn it_works() {
+        let user_id = uuid::Uuid::new_v4();
+        let user_name = "Mi nombre";
+
+        let mut repo = MockCustomRepo::default();
+        repo.expect_get_user().returning(move |id| {
+            let mut user = User::new(user_name.to_string(), (1977, 3, 10));
+            user.id = *id;
+            Ok(user)
+        });
+
+        let /*mut*/ result = get(web::Path::from(user_id), web::Data::new(repo)).await;
+        println!("{:?}", result);
+        // TODO no he conseguido el mock, puede que sea por versiones
+    }
 }
