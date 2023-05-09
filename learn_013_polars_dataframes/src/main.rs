7fa8df76c560a9e00b4396271fe07621f678f1b8
David López <davidlghellin@gmail.com>
2023-05-09 21:23:29 +0200

    [POLARS] [DATA] Configuración y ejemplo de escritura en csv básica

---
+++ learn_013_polars_dataframes/src/main.rs
@@ -0,0 +1,12 @@
+use polars::prelude::*;
+
+fn main() {
+    let mut df = df!(
+        "foo" => &[1, 2, 3],
+        "bar" => &[None, Some("bak"), Some("baz")],
+    )
+    .unwrap();
+    
+    let mut file = std::fs::File::create("path.csv").unwrap();
+    CsvWriter::new(&mut file).finish(&mut df).unwrap();
+}
