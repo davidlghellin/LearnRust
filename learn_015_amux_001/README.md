ab8c6715a20ae02642f0f76a05ddae36668261f7
David López <davidlghellin@gmail.com>
2023-05-13 00:11:31 +0200

    [AXUM] [API] [REST] [WEB] QueryParams

--- learn_015_amux_001/README.md
+++ learn_015_amux_001/README.md
@@ -8,4 +8,11 @@ https://www.youtube.com/watch?v=XZtlD_m59sM
 
 ```sh
 http get 127.0.0.1:8080/hello
+http get 127.0.0.1:8080/hello name=david
+
+curl  -i 'http://localhost:8080/hello?name=david'
+http get "127.0.0.1:8080/hello?name=david"
 ```
+
+
+cargo watch -q -c -w src/ -x run
--- learn_015_amux_001/src/main.rs
+++ learn_015_amux_001/src/main.rs
@@ -2,14 +2,18 @@
 
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::Query,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("<strong>hello world</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello_param));
+
     // region --start server
     let addres = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!(">> Escuchadno en {addres}\n");
@@ -18,4 +22,22 @@ async fn main() {
         .await
         .unwrap();
     // endregion --start server
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:12} - handler_hello", "HANDLER");
+
+    Html("<strong>hello world</strong>")
+}
+
+async fn handler_hello_param(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("world");
+    Html(format!("<strong>hello {name}</strong>"))
 }
