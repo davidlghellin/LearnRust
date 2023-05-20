326ae8b7ec89318be5aece62ed0d5dbb32d5593c
David López <davidlghellin@gmail.com>
2023-05-20 19:42:13 +0200

    [ACTIX] [API] [REST] [WEB] Log

--- learn_016_actix_web_003/README.md
+++ learn_016_actix_web_003/README.md
@@ -13,3 +13,8 @@ docker build -t  todo-app .
 
 docker run --network host -p 8080:8080 -it todo-app
 ```
+
+```shell
+http get 127.0.0.1:8080 
+curl -i  http://127.0.0.1:8080
+```
--- learn_016_actix_web_003/src/main.rs
+++ learn_016_actix_web_003/src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 
 use actix_web::{web, App, HttpServer, Responder};
+use log::info;
+
 
 async fn status() -> impl Responder {
     "{\"status\":  \"UP\"}"
@@ -8,6 +10,12 @@ async fn status() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> io::Result<()>{
+
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+    info!("Activamos logs");
+    info!("Estamos en el 127.0.0.1:8080");
+
     HttpServer::new(
         || App::new().route("/", web::get().to(status))
     ).bind("127.0.0.1:8080")?
