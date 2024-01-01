f1afbed0c6e0421b660d76f0d80bba6c7e180b39
David López <davidlghellin@gmail.com>
2024-01-01 16:25:50 +0100

    [FICHEROS][DOCKER][LOG] Añadimos log en fichero externo para la ejecución de ETL por ejemplo

--- learn_019_read_csv/README.md
+++ learn_019_read_csv/README.md
@@ -10,3 +10,9 @@ Para poder usar el docker podemos hacer uso de los volumenes.
 docker build -t  my-read-csv -f DockerfileScratch .
 docker run -v ~/Documentos/LearnRust/learn_019_read_csv/input/:/input my-read-csv
 ```
+
+Añadimos los log en ficheros (la carpeta destino tiene que existir y tener permisos (bruscamente 777))
+```shell
+docker run -v ~/Documentos/LearnRust/learn_019_read_csv/input/:/input -v ~/Documentos/LearnRust/learn_019_read_csv/log/:/log my-read-csv
+docker run -v ~/Documentos/LearnRust/learn_019_read_csv/input/:/input -v /tmp/:/log my-read-csv
+```
--- learn_019_read_csv/csv-work/src/main.rs
+++ learn_019_read_csv/csv-work/src/main.rs
@@ -1,6 +1,13 @@
 use csv;
+use log::*;
 use serde::Deserialize;
-use std::{error::Error, io};
+use std::error::Error;
+
+use chrono::Local;
+use std::fs::File;
+use std::io::Write;
+
+use std::time::Instant;
 
 /*
 https://crates.io/crates/csv
@@ -15,31 +22,56 @@ struct Record {
     id: Option<u64>,
 }
 
-fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_path(path)?;
-
-    for result in reader.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
-
 fn read_name(path: &str) -> Result<(), Box<dyn Error>> {
+    debug!("Nombre del fichero: {}", path);
     let mut reader = csv::Reader::from_path(path)?;
     // let mut reader = csv::Reader::from_reader(io::stdin()); // cargo run < ../nombres.csv
     for result in reader.deserialize() {
         // Notice that we need to provide a type hint for automatic
         // deserialization.
         let record: Record = result?;
-        println!("{:?}", record);
+        debug!("{:?}", record);
+        println!(
+            "{:?}\t{:?} con id: {:?}",
+            record.nombre,
+            record.apellido,
+            record.id.unwrap()
+        );
     }
     Ok(())
 }
 
 fn main() {
+    // Para guardar en fichero
+    let target = Box::new(File::create("../log/log.log").expect("Can't create file"));
+
+    env_logger::Builder::new()
+        .target(env_logger::Target::Pipe(target))
+        .filter(None, LevelFilter::Debug)
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "[{} {} {}:{}] {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+                record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                record.args()
+            )
+        })
+        .init();
+
+    // FIN Para guardar en fichero
+
+    // std::env::set_var("RUST_LOG", "debug");
+    // env_logger::init();
+    info!("Ini programa");
+
+    let now = Instant::now();
     // if let Err(e) = read_from_file("../customers.csv") {
     if let Err(e) = read_name("../input/nombres.csv") {
         eprintln!("{}", e); // https://doc.rust-lang.org/std/macro.eprintln.html
     }
+    let elapsed = now.elapsed();
+    info!("Fin programa: {:.2?}", elapsed);
 }
