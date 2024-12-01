85b8e753fd48cefd3a45cd2f006f8d50c8597244
David López <davidlghellin@gmail.com>
2024-12-01 22:30:25 +0100

    Ejemplo de polars leyendo de csv y usnado lazyFrames

---
+++ learn_029_polars_ds_salaies/src/main.rs
@@ -0,0 +1,42 @@
+use polars::prelude::*;
+use std::fs::File;
+
+fn main() {
+    // Lee el archivo csv
+    let csv: CsvReader<File> = CsvReadOptions::default()
+        .with_has_header(true)
+        .try_into_reader_with_file_path(Some("ds_salaries.csv".into()))
+        .unwrap();
+
+    // Convierte el archivo csv en un LazyFrame
+    let df: LazyFrame = csv.finish().unwrap().lazy();
+
+    println!("{:?}", df.clone().collect());
+
+    let count: DataFrame = df
+        .clone()
+        .filter(col("employment_type").is_null())
+        .select(&[col("employment_type").count().alias("null count")])
+        .collect()
+        .unwrap();
+
+    println!("{:?}", count);
+
+    let df_group = df
+        .clone()
+        .filter(
+            col("employee_residence")
+                .eq(lit("US"))
+                .and(col("job_title").eq(lit("Data Scientist"))),
+        )
+        .group_by([col("company_location"), col("experience_level")])
+        .agg([
+            col("salary_in_usd").mean().alias("average_salary"),
+            col("salary_in_usd").max().alias("max_salary"),
+            col("salary_in_usd").min().alias("min_salary"),
+        ])
+        .sort_by_exprs(vec![col("experience_level")], Default::default());
+    println!("{:?}", df_group.explain(false));
+    println!("{:?}", df_group.explain(true));
+    println!("{:?}", df_group.collect());
+}
