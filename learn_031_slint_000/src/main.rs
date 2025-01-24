286d2584c913f213722c594f0a7f61a19464aa19
David López <davidlghellin@gmail.com>
2025-01-24 15:57:48 +0100

    [SLINT] [DESKTOP] Calcular impuestos de ejemplo

--- learn_031_slint_000/src/main.rs
+++ learn_031_slint_000/src/main.rs
@@ -1,7 +1,19 @@
 slint::include_modules!();
 
+const TAXPER: f64 = 0.21;
+
 fn main() -> Result<(), slint::PlatformError> {
-    let main_window = MainWindow::new()?;
+    let main_window: MainWindow = MainWindow::new()?;
+    let ui_handler: slint::Weak<MainWindow> = main_window.as_weak();
+
+    main_window.on_divide_income(move |string| {
+        let ui: MainWindow = ui_handler.unwrap();
+        let num: f64 = string.trim().parse().unwrap();
+
+        let tax: f64 = num * TAXPER;
+        let result: String = format!("Impuestos:{}\nGanancia: {}", tax, (num - tax));
+        ui.set_results(result.into());
+    });
 
     main_window.run()
 }
