432c32b35c3d9c4df39a85e3652d6cc920a65df6
David López <davidlghellin@gmail.com>
2025-05-17 16:57:26 +0200

    [DOCKER] [K8S] Ejemplo ping pong incrementando el contador de un número

--- learn_036_basic_actors/actor_server/src/main.rs
+++ learn_036_basic_actors/actor_server/src/main.rs
@@ -1,9 +1,9 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-/// Simula el "Actor" del servidor
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, num: Arc<Mutex<i32>>) {
     let mut buffer = [0; 512];
     loop {
         match stream.read(&mut buffer) {
@@ -13,10 +13,14 @@ fn handle_client(mut stream: TcpStream) {
             }
             Ok(n) => {
                 let message = String::from_utf8_lossy(&buffer[..n]);
-                println!("Server Actor: Received '{}'", message);
+                println!("Server Actor: Received '{}'", message.trim());
 
                 if message.trim() == "Ping" {
-                    stream.write_all(b"Pong\n").unwrap();
+                    // Bloqueamos el mutex para acceder y modificar el contador
+                    let mut counter = num.lock().unwrap();
+                    let response = format!("Pong {}\n", *counter);
+                    stream.write_all(response.as_bytes()).unwrap();
+                    *counter += 1;
                 } else if message.trim() == "Stop" {
                     println!("Server Actor: Stopping as requested.");
                     break;
@@ -34,10 +38,13 @@ fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").expect("Failed to bind to address");
     println!("Server listening on port 7878...");
 
+    let num = Arc::new(Mutex::new(0));
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(|| handle_client(stream));
+                let num_clone = Arc::clone(&num);
+                thread::spawn(move || handle_client(stream, num_clone));
             }
             Err(e) => eprintln!("Connection failed: {:?}", e),
         }
